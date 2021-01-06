a2ea669dd679fd7cf17ad6fdd782194b3ce356be
krandslam <58777020+krandslam@users.noreply.github.com>
2021-01-07 01:10:55 +0900

    Add files via upload

---
+++ day10/module1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def add(num1, num2) :
+    sum1 = num1 + num2
+    
+    return sum1
+
+# In[ ]:
+
+
+def sub(num1, num2) :
+    val = num1 - num2
+    
+    return val
+
+
+# In[ ]:
+
+
+def printA(str1) :
+    print("[ {} ] 님 안녕하세요".format(str1))
+
+
+# In[ ]:
+
+def printB(n) :
+    if n == 1 :
+        print("하위")
+    elif n == 2 :
+        print("안녕")
+    else:
+        print("ㅋ")
+    
+
